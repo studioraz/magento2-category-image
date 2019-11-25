b9fa2af0c2e561bebb6fc0ed0dbf959439887f0a
Roman Lapko <roman.l@naamaraz.com>
2019-11-25 18:26:51 +0200

    SMR-114 - Update image uploader controllers

--- Controller/Adminhtml/Category/Thumbnail/Upload.php
+++ Controller/Adminhtml/Category/Thumbnail/Upload.php
@@ -5,14 +5,102 @@
  */
 namespace SR\CategoryImage\Controller\Adminhtml\Category\Thumbnail;
 
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action;
+use Magento\Catalog\Model\ImageUploader;
+use Magento\MediaStorage\Model\File\UploaderFactory;
+use Magento\MediaStorage\Helper\File\Storage\Database as FileStorageDatabase;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * Class Upload
  */
-class Upload extends \Magento\Catalog\Controller\Adminhtml\Category\Image\Upload
+class Upload extends Action
 {
     const CATEGORY_ATTRIBUTE_IMAGE_THUMBNAIL = 'thumbnail';
+
+    /**
+     * Image uploader
+     *
+     * @var ImageUploader
+     */
+    protected $imageUploader;
+
+    /**
+     * Uploader factory
+     *
+     * @var UploaderFactory
+     */
+    private $uploaderFactory;
+
+    /**
+     * Media directory object (writable).
+     *
+     * @var WriteInterface
+     */
+    protected $mediaDirectory;
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * Core file storage database
+     *
+     * @var \Magento\MediaStorage\Helper\File\Storage\Database
+     */
+    protected $coreFileStorageDatabase;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor
+     *
+     * @param Action\Context $context
+     * @param ImageUploader $imageUploader
+     * @param UploaderFactory $uploaderFactory
+     * @param Filesystem $filesystem
+     * @param StoreManagerInterface $storeManager
+     * @param FileStorageDatabase $coreFileStorageDatabase
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        Action\Context $context,
+        ImageUploader $imageUploader,
+        UploaderFactory $uploaderFactory,
+        Filesystem $filesystem,
+        StoreManagerInterface $storeManager,
+        FileStorageDatabase $coreFileStorageDatabase,
+        LoggerInterface $logger
+    ) {
+        parent::__construct($context);
+
+        $this->imageUploader = $imageUploader;
+        $this->uploaderFactory = $uploaderFactory;
+        $this->mediaDirectory = $filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA);
+        $this->storeManager = $storeManager;
+        $this->coreFileStorageDatabase = $coreFileStorageDatabase;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Check admin permissions for this controller
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('SR_CategoryImage::category');
+    }
 
     /**
      * Upload file controller action
@@ -34,6 +122,7 @@ class Upload extends \Magento\Catalog\Controller\Adminhtml\Category\Image\Upload
         } catch (\Exception $e) {
             $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
         }
+
         return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
     }
 }
